15972116b6ff5f1ac08cf5b20b1e46f6f837b28c
AChin1311 <daphne011552000@yahoo.com.tw>
2018-03-12 21:00:10 -0700

    add parse function

--- para_Lsystem.py
+++ para_Lsystem.py
@@ -1,20 +1,30 @@
-# w = A(100, w0)
-# A(s, w):s>=min ->!(w)F(s)[+(35)/(0)A(s*0.75, w*q^e)][+(-35)/0A(s*0.77, w*(1-q)^e)]
+# w = A(100, 30)
+# A(s, w):s>=0 ->!(w)F(s)[+(35)/(0)A(s*0.75, w*(0.5)^(0.4)))][+(-35)/(0)A(s*0.77, w*(0.5)^(0.4))]
 def get_content(string):
     p = string.find('(')
     q = string.find(')')
 
     return string[:p], string[p+1:q], string[min(q+1, len(string)-1):]
 
+
+def intrp_cond(cond):
+
+def intrp_expr(expr):
+
 def parse(axiom, string, n):
     func, string = string.split(':')
     print func, string
-    name, paras, _ = get_content(func)
-    print name, paras.split(',')
+    func_name, paras, _ = get_content(func)
+    print func_name, paras.split(',')
 
-    condition, expr = string.split('->')
-    print condition, expr
+    cond, expr = string.split('->')
+    print cond, expr
+    
+    cond = intrp_cond(cond)
+    expr = intrp_expr(expr)
+    print cond, expr
 
+    return 
     # for _ in range(n):
 
 
