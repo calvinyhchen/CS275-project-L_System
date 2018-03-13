46addba27cca4864723ffb7f6429e3648f69284f
AChin1311 <daphne011552000@yahoo.com.tw>
2018-03-12 21:21:50 -0700

    parse expression

--- para_Lsystem.py
+++ para_Lsystem.py
@@ -3,25 +3,42 @@
 def get_content(string):
     p = string.find('(')
     q = string.find(')')
-
     return string[:p], string[p+1:q], string[min(q+1, len(string)-1):]
 
 
 def intrp_cond(cond):
+    pass
 
 def intrp_expr(expr):
+# A(s, w):s>=0 -> !(w)F(s)[()+(35)/(0)A(s*0.75, w*0.5^0.4)]()[()+(-35)/(0)A(s*0.77, w*0.5^0.4)]()
+# !, w, F, s, [, , +, 35, / 0, A, (s*0.75, w*(0.5)^(0.4)), ], ,
+# [, , +, -35, /, 0, A, (s*0.77, w*(0.5)^(0.4)), ], 
+    
+    action_list = []
+    expr_list = expr.split(')')
+    print expr_list
+    for ep in expr_list:
+        if ep != '':
+            print ep
+            f = ep[0]
+            p = ep[ep.find('(')+1:]
+            action_list.extend([f, p])
+    print action_list
+    return action_list
+
 
 def parse(axiom, string, n):
-    func, string = string.split(':')
-    print func, string
+
+    func, cond_expr = string.split(':')
+    print func, cond_expr
     func_name, paras, _ = get_content(func)
     print func_name, paras.split(',')
 
-    cond, expr = string.split('->')
+    cond, expr = cond_expr.split('->')
     print cond, expr
     
-    cond = intrp_cond(cond)
-    expr = intrp_expr(expr)
+    # cond = intrp_cond(cond.stip())
+    expr = intrp_expr(expr.strip())
     print cond, expr
 
     return 
